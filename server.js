abb67e74bde6807b748a03d356b0168263e77b3c
Linda Chang <linda_chang@brown.edu>
2018-01-27 16:36:51 -0500

    fixed bugs and can now save to db

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ var itemID = 0;
 
 app.get('*', function(request, response) {
     response.render('index.html');
-    var sql = 'SELECT count(gameID) FROM Game';
-    gameID = conn.query(sql); 
+    var sql = 'SELECT count(gameID) as numGames FROM Game';
+    gameID = conn.query(sql, function(err, res) {
+        gameID = res.rows[0]['numGames'];
+    });; 
 
     var sql = 'SELECT count(achievementID) FROM Achievements';
     achievementID = conn.query(sql); 
@@ -89,89 +91,113 @@ app.post('/createGame', function(req, res) {
 	var clickName = req.body['game']['clickName'];
 	var achievementsTitle = req.body['game']['achievementsTitle'];
 	var storeName = req.body['game']['storeName'];
-	var clickOverride = parseInt(req.body['game']['clickOverride']);
-	var overrideIcon = parseInt(req.body['game']['overrideIcon']);
+	var clickOverride = req.body['game']['clickOverride'];
+	var overrideIcon = req.body['game']['overrideIcon'];
 	var clickSFX = req.body['game']['clickSFX'];
 
-	// write sql query here
+	if (typeof clickOverride == 'undefined') {
+		clickOverride = 0;
+	} else {
+		clickOverride = parseInt(clickOverride);
+	}
+
+	if (typeof overrideIcon == 'undefined') {
+		overrideIcon = 0;
+	} else {
+		overrideIcon = parseInt(overrideIcon);
+	}
+
+	console.log(gameTitle);
+	console.log(defaultImage);
+	console.log(clickName);
+	console.log(achievementsTitle);
+	console.log(storeName);
+	console.log(clickOverride);
+	console.log(overrideIcon);
+	console.log(clickSFX);
+
 
 	gameID = gameID + 1;
 	//GAME
 	var sql = 'INSERT INTO Game (gameID, gameTitle, defaultImage, clickName, achievementsTitle, storeName, clickOverride, overrideIcon, clickSFX) VALUES (\'' 
         + gameID + '\', \'' 
         + gameTitle + '\', \'' 
-        + defaultImage + '\', ' 
-        + clickName + '\', ' 
-        + achievementsTitle + '\', ' 
-        + storeName + '\', ' 
-        + clickOverride + '\', ' 
-        + overrideIcon + '\', '
-        +  clickSFX + ')';
+        + defaultImage + '\',\'' 
+        + clickName + '\',\'' 
+        + achievementsTitle + '\',\'' 
+        + storeName + '\',\'' 
+        + clickOverride + '\',\'' 
+        + overrideIcon + '\',\''
+        +  clickSFX + '\')';
+
+    console.log(sql);
 
     var q = conn.query(sql);
 
+
+
     // ACHIEVEMENTS
     
-    var listAchievements = req.body['achievements'][0];
-    achievementID = achievementID + 1;
+ //    var listAchievements = req.body['achievements'][0];
+ //    achievementID = achievementID + 1;
 
-    for(var achievement of listAchievements) {
+ //    for(var achievement of listAchievements) {
 
-    	var name = achievement['name'];
-    	var image = achievement['image'];
-    	var clicksToUnlock = achievement['clicksToUnlock'];
-    	var changesBigImage = achievement['changesBigImage'];
-    	var newBigImage = achievement['newBigImage'];
-    	var message = achievement['message'];
-    	var achievementID = achievement['achievementID'];
+ //    	var name = achievement['name'];
+ //    	var image = achievement['image'];
+ //    	var clicksToUnlock = achievement['clicksToUnlock'];
+ //    	var changesBigImage = achievement['changesBigImage'];
+ //    	var newBigImage = achievement['newBigImage'];
+ //    	var message = achievement['message'];
+ //    	var achievementID = achievement['achievementID'];
 
-	    sql = 'INSERT INTO Achievements (gameID, name, image, clicksToUnlock, changesBigImage, newBigImage, message, achievementID) VALUES (\'' 
-        + gameID + '\', \'' 
-        + name + '\', \'' 
-        + image + '\', ' 
-        + clicksToUnlock + '\', ' 
-        + changesBigImage + '\', ' 
-        + newBigImage + '\', ' 
-        + message + '\', ' 
-        +  achievementID + ')';
+	//     sql = 'INSERT INTO Achievements (gameID, name, image, clicksToUnlock, changesBigImage, newBigImage, message, achievementID) VALUES (\'' 
+ //        + gameID + '\', \'' 
+ //        + name + '\', \'' 
+ //        + image + '\', ' 
+ //        + clicksToUnlock + '\', ' 
+ //        + changesBigImage + '\', ' 
+ //        + newBigImage + '\', ' 
+ //        + message + '\', ' 
+ //        +  achievementID + ')';
 
-    	q = conn.query(sql);
-    	achievementID = achievementID + 1;
+ //    	q = conn.query(sql);
+ //    	achievementID = achievementID + 1;
 	
-	}
+	// }
 
-	var listItems = req.body['store'];
+	// var listItems = req.body['store'];
+	// itemID = itemID + 1;
+
+	// for(var item of listItems) {
+
+ //    	var image = item['image'];
+ //    	var title = item['title'];
+ //    	var cost = item['cost'];
+ //    	var description = item['description'];
+ //    	var costMultiplier = item['costMultiplier'];
+ //    	var passiveClicks = item['passiveClicks'];
+ //    	var passiveSecs = item['passiveSecs'];
+ //    	var manualClickMultiplier = item['manualClickMultiplier'];
+ //    	var autoClick = item['autoClick'];
+
+ //    sql = 'INSERT INTO Store (gameID, image, title, cost, description, costMultiplier, passiveClicks, passiveSecs, manualClickMultiplier,autoClick,itemID, manualClick) VALUES (\'' 
+ //        + gameID + '\', \'' 
+ //        + image + '\', \'' 
+ //        + title + '\', ' 
+ //        + cost + '\', ' 
+ //        + description + '\', ' 
+ //        + costMultiplier + '\', ' 
+ //        + passiveClicks + '\', ' 
+ //        + passiveSecs + '\', '
+ //        + manualClickMultiplier + '\', '
+ //        + itemID + '\', '
+ //        + autoClick + '\', '
+ //        +  manualClick + ')';
+
+    // q = conn.query(sql);
 	itemID = itemID + 1;
-
-	for(var item of listItems) {
-
-    	var image = item['image'];
-    	var title = item['title'];
-    	var cost = item['cost'];
-    	var description = item['description'];
-    	var costMultiplier = item['costMultiplier'];
-    	var passiveClicks = item['passiveClicks'];
-    	var passiveSecs = item['passiveSecs'];
-    	var manualClickMultiplier = item['manualClickMultiplier'];
-    	var autoClick = item['autoClick'];
-
-    sql = 'INSERT INTO Store (gameID, image, title, cost, description, costMultiplier, passiveClicks, passiveSecs, manualClickMultiplier,autoClick,itemID, manualClick) VALUES (\'' 
-        + gameID + '\', \'' 
-        + image + '\', \'' 
-        + title + '\', ' 
-        + cost + '\', ' 
-        + description + '\', ' 
-        + costMultiplier + '\', ' 
-        + passiveClicks + '\', ' 
-        + passiveSecs + '\', '
-        + manualClickMultiplier + '\', '
-        + itemID + '\', '
-        + autoClick + '\', '
-        +  manualClick + ')';
-
-    q = conn.query(sql);
-	itemID = itemID + 1;
-	}
+	
     
     //response.render('room.html', {roomName: request.params.roomName, nickname: nickname});
 });
