5bf2748d3065ec52360dc6b30a37ad10fa125d2e
Linda Chang <linda_chang@brown.edu>
2018-01-27 12:56:21 -0500

    added views engine

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
+var anyDB = require('any-db');
+var conn = anyDB.createConnection('sqlite3://database.db');
 
 var engine = require('consolidate');
 
+app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
+app.set('views', __dirname + '/templates'); // tell Express where to find templates
+app.use(express.static('public'));
+
 var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/views'));
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
 
-app.get('*', function(request, response){
+app.get('*', function(request, response) {
     response.render('index.html');
 });
+
+
 
 server.listen(8080, function(){
     console.log('- Server listening on port 8080');
